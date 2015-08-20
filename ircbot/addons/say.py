c3d743b6cd4d9b857896df8979110703c31d5167
nulldigit <nulldigit90@gmail.com>
2015-08-20 03:52:18 -0400

    Added say.py

---
+++ ircbot/addons/say.py
@@ -0,0 +1,12 @@
+'''Common command for bots. This one, at random will refuse to blindly
+say what the user wants'''
+
+import random
+
+def main(nick, comargs, chan, send):
+    lol = random.randrange(1,50)
+    if lol > 25 and lol < 35:
+        send.put("How about no. Go fuck yourself %s" % nick)
+    else:
+        phrase = comargs.strip()
+        send.put(phrase)
