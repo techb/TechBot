8c35e9031194f3b1e3e5ef1becab8e7e43eb087b
nulldigit <nulldigit90@gmail.com>
2016-01-19 16:16:26 -0500

    added docstring

--- ircbot/addons/cheers.py
+++ ircbot/addons/cheers.py
@@ -1,3 +1,5 @@
+#written for TechBot by techb
+
 def main(nick, comargs, chan, send):
     '''does a /me with who/what ever is in comargs'''
     # example on sending a simple string to the default channel the bot is on
