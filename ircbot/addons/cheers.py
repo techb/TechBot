9a8d1870eb4401e7e8cbf75aefacb2b14ef6827d
nulldigit <nulldigit90@gmail.com>
2016-01-19 15:55:29 -0500

    cheers added, will tip glass with who comargs is

--- ircbot/addons/cheers.py
+++ ircbot/addons/cheers.py
@@ -1,4 +1,4 @@
 def main(nick, comargs, chan, send):
     '''does a /me with who/what ever is in comargs'''
     # example on sending a simple string to the default channel the bot is on
-    send.put(("\001ACTION tips beer with %s\001" % comargs, chan))
+    send.put(("\001ACTION tips glass of beer with %s\001" % comargs, chan))
