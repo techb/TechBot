89736bc2bafcd8f14dc3160d1d230f6a6c51a3c2
K.B. Carte <13102014+techb@users.noreply.github.com>
2019-05-24 19:56:32 -0400

    Update say.py

--- ircbot/addons/say.py
+++ ircbot/addons/say.py
@@ -6,7 +6,7 @@ import random
 def main(nick, comargs, chan, send):
     lol = random.randrange(1,50)
     if lol > 25 and lol < 35:
-        phrase = "How about no. Go fuck yourself %s" % nick
+        phrase = "How about no. Bruh, %s" % nick
     else:
         phrase = comargs.strip()
 
